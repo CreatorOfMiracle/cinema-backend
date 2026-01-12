c546131445972ba4d0c34a20af5e53b87fb350b6
antonshipelev <antonship12345@gmail.com>
2026-01-12 21:36:11 +0300

    fix

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,14 +5,14 @@ const prisma = new PrismaClient();
 async function main() {
   await prisma.hall.upsert({
     where: { id: 'hall-1' },
-    update: { name: 'Зал 1', capacity: 60 },
-    create: { id: 'hall-1', name: 'Зал 1', capacity: 60 },
+    update: { name: 'Зал (c 60 местами)', capacity: 60 },
+    create: { id: 'hall-1', name: 'Зал (c 60 местами)', capacity: 60 },
   });
 
   await prisma.hall.upsert({
     where: { id: 'hall-2' },
-    update: { name: 'Зал 2', capacity: 90 },
-    create: { id: 'hall-2', name: 'Зал 2', capacity: 90 },
+    update: { name: 'Зал (c 2 местами)', capacity: 2 },
+    create: { id: 'hall-2', name: 'Зал (c 2 местами)', capacity: 2 },
   });
 }
 
